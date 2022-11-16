6feab322ffd478b52df2d98b33d3d1eee8dcb979
MacRude <magnus.1131@hotmail.com>
2022-11-16 12:34:42 +0100

    CreateUserBookmark

---
+++ Portfolio/WebServer/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DataLayer;
+using WebServer.Models;
+
+namespace WebServer.Controllers
+{
+    [Route("api/user")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService = new UserService();
+        [HttpPost("{uid}")]
+        public IActionResult CreateActorBookmark(string userid, string nconstactor, string? usernote)
+        {
+            var bookmark = _userService.CreateActorBookmark(userid, nconstactor, usernote);
+
+            if (bookmark == null)
+            {
+                return NotFound();
+            }
+            var model = new UserBookmarkModel
+            {
+                Url = "http//:localhost/5001/api/user" + bookmark.Nconst,
+                Tconst = bookmark.Tconst,
+                Nconst = bookmark.Nconst,
+                Note = bookmark.Note
+            };
+           
+
+        }
+    }
+}
---
+++ Portfolio/WebServer/Models/UserBookmarkModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebServer.Models
+{
+    public class UserBookmarkModel
+    {
+        public string? Url { get; set; }
+        public string? Tconst { get; set; }
+        public string? Nconst { get; set; }
+        public string? Note { get; set; }
+    }
+}
