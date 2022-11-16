d4ac417371bbdca9069d860421bc0c303a34756f
MacRude <magnus.1131@hotmail.com>
2022-11-16 01:27:04 +0100

    CreateUserServices

---
+++ Portfolio/DataLayer/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer.Domain;
+using DataLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer
+{
+    public interface IUserService
+    {
+        void CreateActorBookmark(string userid, string nconstactor, string usernote);
+        void DeleteActorBookmark(string userid, string nconstactor);
+        void DeleteMovieBookmark(string userid, string tconstmovie);
+        void CreateRating(string userid, string title, int rating);
+        void DeleteRating(string userid, string tconst);
+    }
+}
---
+++ Portfolio/DataLayer/UserService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using DataLayer.Domain;
+using DataLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class UserService : IUserService
+    {
+        public userMain GetUser(string uid)
+        {
+            using var db = new IMDBContext();
+            return db.userMain.Find(uid);
+        }
+        public void CreateActorBookmark(string userid, string nconstactor, string usernote)
+        {
+            var db = new IMDBContext();
+            db.Database.ExecuteSqlInterpolated($"select bookmark_actor({userid},{nconstactor},{usernote})");
+        }
+        public void DeleteActorBookmark(string userid, string nconstactor)
+        {
+            var db = new IMDBContext();
+            db.Database.ExecuteSqlInterpolated($"select delete_bookmark_actor({userid},{nconstactor})");
+        }
+        public void DeleteMovieBookmark(string userid, string tconstmovie)
+        {
+            var db = new IMDBContext();
+            db.Database.ExecuteSqlInterpolated($"select delete_bookmark_movie({userid},{tconstmovie})");
+        }
+        public void CreateRating(string userid, string title, int rating)
+        {
+            var db = new IMDBContext();
+            db.Database.ExecuteSqlInterpolated($"select rate({userid},{title},{rating})");
+        }
+        public void DeleteRating(string userid, string tconst)
+        {
+            var db = new IMDBContext();
+            db.Database.ExecuteSqlInterpolated($"select delete_rate({userid},{tconst})");
+        }
+    }
+}
