3d7505dd626f67fc013580294e75724053c703d2
RasmuskjaerN <rasmusknielsen99@gmail.com>
2022-12-19 17:57:27 +0100

    last

--- Portfolio/WebServer/Models/UserModel.cs
+++ Portfolio/WebServer/Models/UserModel.cs
@@ -29,7 +29,7 @@ namespace WebServer.Models
     }
     public class UserCreateBookmark
     {
-        public int UserId { get; set; }
+        public int Uid { get; set; }
         public string Tconst { get; set; }
         public string? Note { get; set; }
     }
