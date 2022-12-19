a7c82116ccde9658c949e5e3bcbe5054dc31b246
jakobhh <jakob@hoefsten.dk>
2022-12-19 23:06:13 +0100

    added-missing-files

---
+++ frontend/src/UserHistory.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { userID } from  './UserHandler.js';
+
+function Bookmarks() {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const uid = userID;
+        const url = `http://localhost:5001/api/user/` + uid;
+        const response = await fetch(url/*, {
+          headers: {
+            'Authorization': 'Bearer ' + token
+          }
+        }*/);
+        const data = await response.json(); 
+        setUser(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+      }
+    }
+    fetchData();
+  }, []);
+
+  console.log(user);
+  return (
+    <div>
+      {error ? (
+        <div>{error}</div>
+      ) : (
+        loading || !user ? (
+          <div>Loading...</div>
+        ) : (
+          <div>
+            {user.histories
+              .slice(-10)
+              .map((history, index) => (
+                <div key={index}>{history.historyDate} | {history.historySearchInput}</div>
+              ))}
+          </div>
+        )
+      )}
+    </div>
+  );
+}
+
+export default Bookmarks;
