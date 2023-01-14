05f2ec7026d933f1a7f41796e48bdd3d308cebe2
jakobhh <jakob@hoefsten.dk>
2023-01-14 20:38:01 +0100

    Revert "a"
    This reverts commit 18d0c0616ff6789f92c9c3213fe8e233ce3cc264.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.js";
-import SideBoxes from "./SideBoxes";
+import SideBoxes from "./sideBoxes";
 
 import reportWebVitals from "./reportWebVitals";
 
