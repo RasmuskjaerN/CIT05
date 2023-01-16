47672b7fc695720e41c86e9d1f063335406f18be
Rasmus Kjær Nielsen <rasmusknielsen99@gmail.com>
2023-01-16 09:25:28 +0100

    typo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,17 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.js";
-import SideBoxes from "./SideBoxes";
+import SideBoxes from "./sideBoxes";
 
 import reportWebVitals from "./reportWebVitals";
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <div>
-      <App/>
-      <SideBoxes/>
+      <App />
+      <SideBoxes />
     </div>
   </React.StrictMode>
 );
